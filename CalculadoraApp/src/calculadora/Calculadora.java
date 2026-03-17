cc858ddbb8045d9c01804b02a87080638cfa4972
Tarde <Tarde@DAW115>
2026-03-17 17:24:36 +0100

    metodo de division

--- CalculadoraApp/src/calculadora/Calculadora.java
+++ CalculadoraApp/src/calculadora/Calculadora.java
@@ -8,4 +8,5 @@ public class Calculadora {
 	 System.out.println("--- INICIO DE LA CALCULADORA ---");
 	 System.out.println("Suma: " + calc.sumar(10, 5));
 	 }
+	 public double dividir(double a, double b) { return a / b; }
 	}
